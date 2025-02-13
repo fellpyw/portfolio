8f709d4b010f33fcbf89dd01bce44a7a8a3cbd53
felipão <feliperodvrt@gmail.com>
2025-02-13 19:42:33 -0300

    13/02

--- index.php
+++ index.php
@@ -1,13 +1,43 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfólio</title>
-
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <div class="interface">
+
+        <header class="cabecalho">
+            <nav class="menu">
+                <div class="logo">
+                    <img src="imagens/logo.png" alt='LOGO' width='150'>
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>Serviços</li>
+                    <li>Produtos</li>
+                    <li>Portfólio</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+
+
+            <div class="titles">
+                <h1 class="title">Entregando Serviços de Qualidade</h1>
+                <h6 class="subtitle">Soluções em T.I</h6>
+                <p class='paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur natus obcaecati, illo doloribus iusto sit numquam nam laboriosam?</p>
+            </div>
+
+            <div class="socialIcons">
+                <a href="#" target="_blank">Twitter</a>
+                <a href="#">Facebook</a>
+                <a href="#">LinkedIn</a>
+                <a href="#">Instagram</a>
+            </div>
+        </header>
+        
+    </div>
 </body>
 </html>
