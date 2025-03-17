4da63c521938e5fa54746622fcc183eac6e15793
VG_VITIN <feliperodvrt@gmail.com>
2025-03-17 19:36:45 -0300

    .

--- portfólio/index.php
+++ portfólio/index.php
@@ -3,12 +3,10 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Portfólio</title>
+    <title>Portifólio</title>
 
     <link rel="stylesheet" href="tools/fontawesome/css/all.css" />
-    <link rel="stylesheet" href="css/style.css" />
-
-    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="css/estilos.css" />
 
 </head>
 <body>
@@ -26,39 +24,92 @@
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Serviços</a></li>
-                <li><a href="#">Produtos</a></li>
-                <li><a href="#">Portfólio</a></li>
+                <li><a href="#">Portifólio</a></li>
                 <li><a href="#">Sobre</a></li>
                 <li><a href="#">Contato</a></li>
+                <li><a href="#">Restrito</a></li>
             </ul>
 
         </nav>
-<!-- ============================================================= -->
-
-        <div class="titles">
-
+        
+        <div class="titles"> <!-- ========================= -->
             <div class="container-titles">
-
-                <h1 class="titulo">Entregando serviços de qualidade</h1>
+                <h1 class="titulo">Entregando Serviços de Qualidade</h1>
                 <h1 class="subtitulo">Soluções em TI</h1>
 
-                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio deleniti nisi rem nihil excepturi cupiditate.</p>
-
+                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae rerum, necessitatibus quod quaerat mollitia.</p>
+                
             </div>
 
             <div class="socialIcons">
-
                 <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
                 <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
                 <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
                 <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
-
             </div>
-        </div>
-
-<!-- ============================================================= -->
-
+        </div> <!-- ========================== -->
+        
     </header>
+
+    <!-- ========================================================= -->
+
+    <main id="corpo">
+
+        <section class="servicos">
+
+            <article class="servico">
+                <img src="imagens/service-design.svg" alt="Design">
+                <h3 class="title-servico">Desenvolvimento WEB</h3>
+                <p class="text-servico">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis omnis, voluptas error impedit consectetur vitae.</p>
+            </article>
+
+            <article class="servico">
+                <img src="imagens/service-hosting.svg" alt="Hosting">
+                <h3 class="title-hosting">WEB Hosting</h3>
+                <p class="text-servico">Possimus vel temporibus deleniti repudiandae in similique unde necessitatibus odit corporis nihil soluta, ad repellendus?</p>
+            </article>
+
+            <article class="servico">
+                <img src="imagens/service-social.svg" alt="Social Media">
+                <h3 class="title-servico">Mídias Sociais</h3>
+                <p class="text-servico">Earum mollitia a, quaerat magni dignissimos, sint velit libero minus magnam saepe perferendis aspernatur? Libero!</p>
+            </article>
+
+            <article class="servico">
+                <img src="imagens/service-seo.svg" alt="SEO">
+                <h3 class="title-servico">Aplicações SEO</h3>
+                <p class="text-servico">Maiores aliquid sequi expedita. Rerum itaque nihil maxime at explicabo similique recusandae saepe magnam a!</p>
+            </article>
+
+            <article class="servico">
+                <img src="imagens/service-cloud.svg" alt="Cloud">
+                <h3 class="title-servico">Computação em Nuvem</h3>
+                <p class="text-servico">Provident velit fuga consequuntur quae, iste accusamus repellendus, officiis quibusdam, odio doloribus voluptates iusto perspiciatis.</p>
+            </article>
+            
+            <article class="servico">
+                <img src="imagens/service-secure.svg" alt="Security">
+                <h3 class="title-servico">Segurança de Dados</h3>
+                <p class="text-servico">Expedita illo fugit error in quidem qui, architecto eligendi quae, perspiciatis modi non ab aliquid.</p>
+            </article>
+
+        </section>
+
+        <section class="portifolio">
+
+            <h3 class="title-portifolio">Nosso Portifólio</h3>
+            <p class="sub-title">Lorem ipsum dolor sit amet.</p>
+
+        </section>
+
+        <section class="sobre">
+
+
+
+        </section>
+
+
+    </main>
 
 </div>
     
