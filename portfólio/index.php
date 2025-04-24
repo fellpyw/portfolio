6d1239a688a707fa2631bef36f203d12e422a03b
Felipão <feliperodvrt@gmail.com>
2025-04-24 19:50:00 -0300

    .

--- portfólio/index.php
+++ portfólio/index.php
@@ -6,7 +6,13 @@
     <title>Portifólio</title>
 
     <link rel="stylesheet" href="tools/fontawesome/css/all.css" />
-    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/estilos.css" />
+
+    <script src="tools/jquery/jquery.js"></script>
+
+    <script>
+        
+    </script>
 
 </head>
 <body>
@@ -22,12 +28,12 @@
             </div>
 
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Portifólio</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Contato</a></li>
-                <li><a href="#">Restrito</a></li>
+                <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
+                <li><a href="#servicos"><i class="fas fa-tasks"></i>Serviços</a></li>
+                <li><a href="#"><i class="fas fa-photo-video"></i>Portifólio</a></li>
+                <li><a href="#"><i class="fas fa-user-friends"></i>Sobre</a></li>
+                <li><a href="#"><i class="fas fa-comments"></i>Contato</a></li>
+                <li><a href="area_restrita/"><i class="fas fa-unlock-alt"></i>Restrito</a></li>
             </ul>
 
         </nav>
@@ -55,7 +61,7 @@
 
     <main id="corpo">
 
-        <section class="servicos">
+        <section id="servicos" class="servicos">
 
             <article class="servico">
                 <img src="imagens/service-design.svg" alt="Design">
@@ -142,43 +148,113 @@
 
             <h3 class="title-contato">Fale conosco</h3>
             <p class="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            
+
             <div class="container-contato">
 
                 <aside class="dados-contato">
-
                     <h4 class="title-aside">Endereço</h4>
-                    <p class="txt-aside">Rua das Margaridas, S/Nº - Centro - Manhuaçu/MG</p>
+                    <p class="txt-aside">Rua das Margaridas, S/Nº<br />Centro - Manhuaçu/MG</p>
 
                     <h4 class="title-aside">Telefone</h4>
                     <p class="txt-aside">33 3331-3333</p>
-                    <p class="txt-aside">33 98400-9999</p>
+                    <p class="txt-aside">33 9 8400-9999</p>
 
-                    <h4 class="title-aside">E-mail</h4>
+                    <h4 class="title-aside">E-Mail</h4>
                     <p class="txt-aside">contato@meusite.com.br</p>
                     <p class="txt-aside">faleconosco@meusite.com.br</p>
-
                 </aside>
 
-                <form action="" method="post">
+                <form action="" method="POST">
 
-                    <input type="text" name="nome" id="" placeholder="Nome *" required />
-                    <input type="email" name="email" id="" placeholder="E-Mail *" required />
-                    <input type="text" name="assunto" id="assunto" placeholder="Assunto" />
-                    <textarea name="mensagem" id="mensagem" placeholder="Mensagem *" required></textarea>
+                    <div class="line-form">
+                        <input type="text" name="nome" id="nome" placeholder="Nome*" required />
+                        <input type="email" name="email" id="email" placeholder="E-Mail*" required />
+                    </div>
 
-                    <input type="submit" value="Enviar Mensagem" />
+                    <div class="line-form">
+                        <input type="text" name="assunto" id="assunto" placeholder="Assunto" />
+                    </div>
+
+                    <div class="line-form">
+                        <textarea name="mensagem" id="mensagem" rows="7" placeholder="Escreva Sua Mensagem*" required></textarea>
+                    </div>
+
+                    <button type="submit">Enviar mensagem</button>
 
                 </form>
 
             </div>
 
-
-
         </section>
 
-
     </main>
+
+    <!-- ========================================================= -->
+
+    <footer id="rodape">
+
+        <div class="sombreamento">
+
+            <div class="container-rodape">
+
+                <section class="item-rodape sobre">
+
+                    <img src="imagens/logo.png" alt="Logo" />
+
+                    <p class="txt-sobre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci reiciendis itaque nihil magni excepturi, assumenda in dolore iure necessitatibus deserunt.</p>
+
+                    <h4 class="title-sobre">Siga-nos</h4>
+
+                    <div class="social-icons">
+                        <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                        <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                        <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
+                        <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+                    </div>
+
+                </section>
+
+                <section class="item-rodape servicos">
+
+                    <h4 class="title-servicos">Serviços</h4>
+
+                    <ul>
+                        <li><a href="#">Desenvolvimento WEB</a></li>
+                        <li><a href="#">WEB Hosting</a></li>
+                        <li><a href="#">Mídias Sociais</a></li>
+                        <li><a href="#">Aplicações SEO</a></li>
+                        <li><a href="#">Computação em Nuvem</a></li>
+                        <li><a href="#">Segurança de Dados</a></li>
+                    </ul>
+
+                </section>
+
+                <section class="item-rodape contato">
+
+                    <h4 class="title-contato">Contatos</h4>
+
+                    <div class="container-contato">
+                        <p class="txt-contatos">Rua das Margaridas, S/Nº</p>
+                        <p class="txt-contatos">33 9 8400-9999</p>
+                        <p class="txt-contatos">contato@meusite.com.br</p>
+                    </div>
+
+                    <h4 class="title-contato">Newsletter</h4>
+                    <form action="" method="GET">
+                        <input type="email" name="news-email" id="news-email" placeholder="E-Mail" />
+
+                        <button type="submit"><i class="fab fa-telegram"></i></button>
+                    </form>
+
+                </section>
+
+            </div>
+
+            <p class="txt-creditos">Geraldo Cezário Neto &copy; 2025</p>
+
+        </div>
+
+    </footer>
 
 </div>
     
